ec05ce18cf216aa035d76c50c933b063fbabae23
root <roman.mandeleil@gmail.com>
2015-04-15 07:57:32 -0400

    Remove MyAccount protection

--- MyAccount.sol
+++ MyAccount.sol
@@ -4,15 +4,16 @@ contract MyAccount{
    bytes32 name = "Roman";
    address owner = 0xcd2a3d9f938e13cd947ec05abc7fe734df8dd827;
    uint balance = 1000000;
-      
+   
+
    function addToBalance(uint to_add){
 		balance = balance + to_add;   
    }
 
    function subFromBalance(uint to_sub){
-        if (msg.sender != owner) return;
         balance = balance - to_sub;  
    }
+
 
 }
 
